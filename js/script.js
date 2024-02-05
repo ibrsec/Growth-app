32d8f663d32140434bdb42c79e27ae6328b1bc8d
ibr <ibr.seckin@gmail.com>
2024-02-05 12:34:26 +0300

    js end

--- js/script.js
+++ js/script.js
@@ -1,20 +1,36 @@
-const hamburgerBtn =  document.querySelector(".hamburger");
+
+
+
+
+
+const hamburgerBtn = document.querySelector(".hamburger");
 const openMenu = document.getElementById("hamburger-menu");
+const questionBtns = document.querySelectorAll(".question-buttons button");
+const qaContainers = document.querySelectorAll(".qa-cont");
 
-const questionBtns =  document.querySelectorAll(".question-buttons button");
 
-
-hamburgerBtn.addEventListener("click",function(){
-    openMenu.classList.toggle("open");
+hamburgerBtn.addEventListener("click", function () {
+  openMenu.classList.toggle("open");
 });
 
+questionBtns.forEach((btn) => {
+  btn.addEventListener("click", function () {
+    for (let i = 0; i < questionBtns.length; i++) {
+      questionBtns[i].classList.remove("blue-btn");
+    }
+    btn.classList.toggle("blue-btn");
+  });
+});
 
-
-questionBtns.forEach((btn) =>{
-    btn.addEventListener("click",function(){
-        for(let i = 0; i < questionBtns.length ; i ++){
-            questionBtns[i].classList.remove("blue-btn");
-        }
-        btn.classList.toggle("blue-btn");
-    });
+qaContainers.forEach((each) => {
+  each.querySelector(".q-cont").addEventListener("click", function () {
+    for (let i = 0; i < qaContainers.length; i++) {
+      qaContainers[i].querySelector(".a-cont").classList.remove("open");
+      qaContainers[i].querySelector("i").classList.remove("fa-minus");
+      qaContainers[i].querySelector("i").classList.add("fa-plus");
+    }
+    each.querySelector(".a-cont").classList.toggle("open");
+    each.querySelector("i").classList.remove("fa-plus");
+    each.querySelector("i").classList.add("fa-minus");
+  });
 });
